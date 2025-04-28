5c43ee73567b8d2f0a5a0a06845cb65e9feac1c3
public-release <as-public-release@janestreet.com>
2025-04-28 19:35:05 +0100

    v0.18~preview.130.26+1192

---
+++ test/inline/ppx_let_test.ml
@@ -0,0 +1,6 @@
+module Local_option = Local_option
+module Locality = Locality
+module Test_local = Test_local
+module Test_location_in_scope = Test_location_in_scope
+module Test_op_n = Test_op_n
+module Test_while = Test_while
